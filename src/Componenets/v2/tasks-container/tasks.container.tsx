33d0286fa01ce3ebd4686fa4d0cb4760405321d2
Dylan Lewis <42690452+BlobDylan@users.noreply.github.com>
2024-11-08 10:42:26 +0200

    Nightnight (#5)
    * lilatov
    
    * display shavtzak before merge

--- src/Componenets/v2/tasks-container/tasks.container.tsx
+++ src/Componenets/v2/tasks-container/tasks.container.tsx
@@ -1,4 +1,26 @@
-import { Box, Button, Pagination, Stack, Typography } from "@mui/material";
+import type { FC } from "react";
+import {
+  Box,
+  Button,
+  Pagination,
+  Stack,
+  Typography,
+  Grid2 as Grid,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+
+import CloseIcon from "@mui/icons-material/Close";
+
 import {
   CompanyContextType,
   useCompanyContext,
@@ -7,8 +29,93 @@ import { useEffect, useState } from "react";
 import { MissionDay } from "../../shared/MissionDay.model";
 import { TaskInstance } from "../../shared/Task.model";
 import { Task } from "../../task-components/Task";
+import { Task as TaskModel } from "../../shared/Task.model";
 import "./tasks.container.scss";
 import ExceptionsComponent from "./exceptions-component/exceptions.component";
+import { Soldier } from "../../shared/Soldier.model";
+
+const DisplayTasksSummary: FC<{ missionDay: MissionDay }> = ({
+  missionDay,
+}) => {
+  const companyContext = useCompanyContext() as CompanyContextType;
+
+  const uniqueTasks = companyContext.getUniqueTasks();
+  return (
+    <>
+      {uniqueTasks.map((task: TaskModel) => (
+        <DisplayTaskSummary task={task} missionDay={missionDay} />
+      ))}
+    </>
+  );
+};
+
+const DisplayTaskSummary: FC<{ task: TaskModel; missionDay: MissionDay }> = ({
+  task,
+  missionDay,
+}) => {
+  const companyContext = useCompanyContext() as CompanyContextType;
+
+  const taskinstances =
+    companyContext.company.getRelevantTaskInstances(missionDay);
+
+  const platoonColors: { [key: number]: string } = {
+    1: "#fdefc0",
+    2: "#d9f3d0",
+    3: "#e3d0f0",
+    4: "primary.main",
+  };
+
+  return (
+    <TableContainer component={Paper}>
+      <Typography align="center" marginTop={1}>
+        {task.type}
+      </Typography>
+      <Table sx={{ minWidth: 800 }} aria-label="simple table">
+        <TableRow>
+          {task.roles.map((role: any) => (
+            <TableCell variant="head" align="center">
+              {role}
+            </TableCell>
+          ))}
+          <TableCell variant="head" align="center">
+            שעות
+          </TableCell>
+        </TableRow>
+        <TableBody>
+          {taskinstances.map(
+            (taskInstance: TaskInstance) =>
+              taskInstance.task.type === task.type && (
+                <TableRow>
+                  {taskInstance.assignedSoldiers.map((soldier: Soldier) => (
+                    <TableCell
+                      sx={{
+                        backgroundColor: platoonColors[soldier.platoon],
+                        color: "black",
+                      }}
+                      size="small"
+                      align="right"
+                    >
+                      {soldier.name}
+                    </TableCell>
+                  ))}
+                  <TableCell
+                    sx={{
+                      backgroundColor: "text.primary",
+                      color: "black",
+                    }}
+                    size="small"
+                    align="right"
+                  >
+                    {taskInstance.startTime.toLocaleTimeString()}
+                  </TableCell>
+                </TableRow>
+              )
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
 
 function TasksContainer() {
   const [currentMissionDay, setMissionDay] = useState<MissionDay | null>();
@@ -18,6 +125,15 @@ function TasksContainer() {
     useState<TaskInstance[]>([]);
   const [shouldShowExceptions, setShouldShowExceptions] =
     useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     const today = new Date();
@@ -30,7 +146,7 @@ function TasksContainer() {
     setCurrentPage(
       companyContext.company.missionDays.indexOf(currentMissingDayResult) + 1
     );
-  }, [companyContext.company]);
+  }, [companyContext]);
 
   useEffect(() => {
     if (currentMissionDay) {
@@ -61,54 +177,56 @@ function TasksContainer() {
     setCurrentPage(value);
   };
 
-  if (currentMissionDay === null) return <div>error</div>;
+  if (currentMissionDay === null || currentMissionDay === undefined)
+    return <div>error</div>;
   else if (currentMissionDay)
     return (
-      <div>
-        <Box
-          width={"100%"}
-          display={"flex"}
-          flexDirection={"column"}
-          alignItems={"center"}
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        alignItems={"center"}
+        sx={{ padding: "1rem" }}
+        width="100%"
+      >
+        <Typography variant="h4">Tasks</Typography>
+        <Typography variant="h6">
+          {currentMissionDay.startOfDay.toDateString()}
+        </Typography>
+        {shouldShowExceptions && (
+          <ExceptionsComponent
+            missionDay={currentMissionDay}
+          ></ExceptionsComponent>
+        )}
+        <Button
+          sx={{ backgroundColor: "primary.main" }}
+          onClick={() => {
+            setShouldShowExceptions(!shouldShowExceptions);
+          }}
         >
-          <Typography variant="h4">Tasks</Typography>
-          <Typography variant="h6">
-            {currentMissionDay.startOfDay.toDateString()}
-          </Typography>
-          <div className="shared-tasks">
-            <div className="select-exceptions-button">
-              <Button
-                sx={{ backgroundColor: "primary.main" }}
-                onClick={() => {
-                  setShouldShowExceptions(!shouldShowExceptions);
-                }}
-              >
-                Toggle exceptions select
-              </Button>
-            </div>
-          </div>
-          {shouldShowExceptions && (
-            <ExceptionsComponent
-              missionDay={currentMissionDay}
-            ></ExceptionsComponent>
-          )}
-          {!shouldShowExceptions && (
-            <div>
+          Toggle exceptions select
+        </Button>
+        {!shouldShowExceptions && (
+          <>
+            <Stack spacing={1}>
               {currentMissionDayTaskInstances.length == 0 && (
-                <div>
-                  <div className="no-tasks-title">
-                    No tasks exists on this mission day
-                  </div>
-                  <div className="generate-default-tasks-button">
-                    <Button
-                      onClick={onClickedGenerateDefaultTasks}
-                      sx={{ backgroundColor: "primary.main" }}
-                    >
-                      Generate default tasks
-                    </Button>
-                  </div>
-                </div>
+                <Box
+                  display={"flex"}
+                  flexDirection={"column"}
+                  alignItems={"center"}
+                >
+                  <Typography>No tasks exists on this mission day</Typography>
+                  <Box flexGrow={1}></Box>
+                  <Button
+                    onClick={onClickedGenerateDefaultTasks}
+                    sx={{
+                      backgroundColor: "primary.main",
+                    }}
+                  >
+                    Generate default tasks
+                  </Button>
+                </Box>
               )}
+              <Box flexGrow={1}></Box>
               {!(currentMissionDayTaskInstances.length == 0) && (
                 <Button
                   onClick={onClickedGenerateAssignment}
@@ -117,23 +235,54 @@ function TasksContainer() {
                   Generate Assignment
                 </Button>
               )}
+            </Stack>
+            <Grid container rowSpacing={1} columns={12} columnSpacing={1}>
               {currentMissionDayTaskInstances.map((taskInstance, index) => (
-                <Task key={index} taskInstance={taskInstance} />
+                <Grid
+                  size={{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12 }}
+                  key={index}
+                >
+                  <Task taskInstance={taskInstance} />
+                </Grid>
               ))}
-            </div>
-          )}
-          <Stack spacing={2} display={"flex"} alignItems={"center"}>
-            <Pagination
-              count={companyContext.company.missionDays.length}
-              variant="outlined"
-              shape="rounded"
-              sx={{ backgroundColor: "primary.main" }}
-              onChange={handlePageChange}
-              page={currentPage}
-            />
-          </Stack>
-        </Box>
-      </div>
+            </Grid>
+          </>
+        )}
+        <Stack
+          spacing={1}
+          display={"flex"}
+          alignItems={"center"}
+          margin={"10px"}
+        >
+          <Button
+            sx={{ backgroundColor: "primary.main", marginTop: "1rem" }}
+            onClick={handleClickOpen}
+          >
+            Display Shavtzak
+          </Button>
+          <Dialog open={open} keepMounted fullScreen onClose={handleClose}>
+            <DialogTitle align="center">{"שבצק פלוגה ב"}</DialogTitle>
+            <Button
+              onClick={handleClose}
+              sx={{ position: "absolute", right: "10px", top: "10px" }}
+            >
+              <CloseIcon />
+            </Button>
+            <DialogContent>
+              <DisplayTasksSummary missionDay={currentMissionDay} />
+            </DialogContent>
+            <DialogActions></DialogActions>
+          </Dialog>{" "}
+          <Pagination
+            count={companyContext.company.missionDays.length}
+            variant="outlined"
+            shape="rounded"
+            sx={{ backgroundColor: "primary.main" }}
+            onChange={handlePageChange}
+            page={currentPage}
+          />
+        </Stack>
+      </Box>
     );
 }
 
